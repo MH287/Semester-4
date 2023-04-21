54264f497bb01bdca3148b80096e595567c37115
MH287 <90404200+MH287@users.noreply.github.com>
2023-04-21 16:54:09 +0200

    MausController Update

--- Assets/Scripts/Marek/Player/MouseController.cs
+++ Assets/Scripts/Marek/Player/MouseController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using StarterAssets;
 using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
+    [SerializeField] private StarterAssetsInputs _starterAssetsInputs;
+
     public void LockMouse()
     {
+        _starterAssetsInputs.cursorInputForLook = true;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void FreeMouse()
     {
+        _starterAssetsInputs.cursorInputForLook = false;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
     }
