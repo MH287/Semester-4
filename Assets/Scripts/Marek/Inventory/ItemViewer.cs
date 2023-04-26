ad73cd94ee4bd1c0d1f44c01fc34d867a7f1bb5a
MH287 <90404200+MH287@users.noreply.github.com>
2023-04-26 12:16:28 +0200

    Itemviewer

--- Assets/Scripts/Marek/Inventory/ItemViewer.cs
+++ Assets/Scripts/Marek/Inventory/ItemViewer.cs
@@ -5,26 +5,26 @@ using UnityEngine.EventSystems;
 
 public class ItemViewer : MonoBehaviour, IDragHandler
 {
-    [SerializeField] private GameObject _viewerCanvas;
     [SerializeField] private Item _item;
-    private GameObject _itemPrefab;
+    [SerializeField] private GameObject _itemPrefab;
 
     public void Spawn3DItem()
      {
-         /*if(_itemPrefab != null)
+         if(_itemPrefab != null)
          {
              Destroy(_itemPrefab.gameObject);
-         }*/
-         
-         _viewerCanvas.SetActive(true); 
+         }
+
+
          _itemPrefab = Instantiate(_item.Prefab, new Vector3(1000, 1000, 1000), Quaternion.identity);
+
      }
 
      public void OnDrag(PointerEventData eventData)
      {
          Debug.Log("OnDrag");
-         //_item.Prefab.transform.eulerAngles += new Vector3(-eventData.delta.y, -eventData.delta.x);
-         _item.Prefab.transform.rotation = Quaternion.Euler(eventData.delta.y, eventData.delta.x, 0);
+         _itemPrefab.transform.eulerAngles += new Vector3(-eventData.delta.y, -eventData.delta.x);
+         //_itemPrefab.transform.rotation = Quaternion.Euler(eventData.delta.y, eventData.delta.x, 0);
      }
 }
 
